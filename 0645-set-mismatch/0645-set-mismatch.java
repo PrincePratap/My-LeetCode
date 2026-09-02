eac332aae2378c3ec1e439f1fdf9a76ce43b3656
Prince Pratap Singh <Princepratap202@gmail.com>
2026-09-02 22:21:19 +0530

    Time: 14 ms (33.22%), Space: 47.8 MB (34.58%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        int duplicate = -1;
+        int missing = -1;
+        HashSet<Integer>  set = new HashSet<>();
+        // find duplicate
+        for(int num : nums){
+            if(set.contains(num)){
+                duplicate = num;
+            }
+            set.add(num);
+        }
+        for(int i=1; i<=nums.length; i++){
+            if(!set.contains(i)){
+                missing = i;
+            }
+        }
+        return new int[]{duplicate,missing};
+
+
+        
+    }
+}
